5e98ad8fd7fa005add1cc374ce2806a826192040
John Sugden <7507362+johnsug@users.noreply.github.com>
2018-01-11 00:19:39 -0600

    Update leaflet_map.R

--- leaflet_map.R
+++ leaflet_map.R
@@ -1,2 +1,15 @@
 
+library(leaflet)
+library(data.table)
 
+d <- fread("https://raw.githubusercontent.com/johnsug/maps/master/data.csv")
+
+d[, .N, by=Comment]
+
+leaflet(d) %>% 
+  addTiles() %>% 
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addCircleMarkers(data=d[Comment=="Residence"], ~Long, ~Lat, radius=10, stroke=F, color="blue") %>%
+  addCircleMarkers(data=d[Comment=="Residence"], ~Long, ~Lat, radius=3, fillOpacity=0.5, stroke=F, color="blue") %>%
+  addCircleMarkers(data=d[Comment %in% c("Visit", "Interview")], ~Long, ~Lat, radius=3, stroke=F, fillOpacity=0.5, color="red") %>%
+  addCircleMarkers(data=d[Comment=="Airport"], ~Long, ~Lat, radius=3, fillOpacity=0.5, stroke=F, color="orange")
